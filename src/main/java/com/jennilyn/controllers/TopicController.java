3a00528ca3190d692ce56be25e82375ead0bfe1d
Jennilyn Howell <jennilynhowell@me.com>
2017-07-31 10:13:14 -0400

    created model and displayed content to index

--- src/main/java/com/jennilyn/controllers/TopicController.java
+++ src/main/java/com/jennilyn/controllers/TopicController.java
@@ -1,13 +1,28 @@
 package com.jennilyn.controllers;
 
+import com.jennilyn.models.Topic;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class TopicController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        Topic first = new Topic(1, "JavaSpring", "Let's learn Spring");
+        Topic second = new Topic(2, "Node", "How do we use JS on the back end?");
+        Topic third = new Topic(3, "React", "What's a single-page app, anyway?");
+
+        ArrayList<Topic> topics = new ArrayList<>();
+        topics.add(first);
+        topics.add(second);
+        topics.add(third);
+
+        model.addAttribute("topics", topics);
+
         return "index";
     }
 
