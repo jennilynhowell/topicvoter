8c16b6808ed0ef1bc6459c3dcd3eb2765a56e970
Jennilyn Howell <jennilynhowell@me.com>
2017-07-31 09:40:24 -0400

    Initial commit - basics set up

---
+++ src/main/java/com/jennilyn/controllers/TopicController.java
@@ -0,0 +1,15 @@
+package com.jennilyn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TopicController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+}
